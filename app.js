70020ea92549b16ee0c0e8a0dcffed8d75ca8203
Sujal4927 <sujalwaghmare8471@gmail.com>
2026-05-01 18:17:03 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const form = document.querySelector("#searchForm");
+form.addEventListener("submit", async function (e) {
+    e.preventDefault();
+    const searchTerm = form.elements.query.value;
+    const config = { params: { q: searchTerm } };
+    const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+    makeImages(res.data);
+    form.elements.query.value = '';
+})
+
+
+const resultImages = document.querySelector("#resultImages");
+const makeImages = (shows) => {
+    resultImages.innerHTML = '';
+    for (let result of shows) {
+        if (result.show.image) {
+            const Container = document.createElement("div");
+            Container.classList.add("first");
+            // resultImages.appendChild(Container);
+
+            Container.innerHTML = `
+                    <div class="card m-3" style="width: 15rem; border-radius: 14px; background-color: #48828b; border: none; cursor: pointer;">
+                        <img src="${result.show.image.medium}" class="card-img-top" style="border-radius: 14px 14px 0 0;" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title" style="color: #04172e">${result.show.name}</h5>
+                                <a href="#" class="btn px-2" style="border-radius: 50%; color: #fff; background-color: #04172e;"> ${result.show.rating.average}</a>
+                            </div>
+                </div>`
+
+            resultImages.appendChild(Container);
+
+
+            // const img = document.createElement('IMG');
+            // img.src = result.show.image.medium;
+            // resultImages.appendChild(img);
+
+        }
+    }
+}
